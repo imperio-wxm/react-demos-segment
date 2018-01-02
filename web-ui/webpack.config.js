f22052b72c5e90897cc1898918e5995ded730f66
wxmimperio <wxmimperio@outlook.com>
2018-01-02 15:26:25 +0800

    init web-ui

---
+++ web-ui/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  output: {
+    path: __dirname + "/public",//打包后的文件存放的地方
+    filename: "bundle.js"//打包后输出文件的文件名
+  },
+  devServer: {
+    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    historyApiFallback: true,//不跳转
+    inline: true,//实时刷新
+	port: 8900
+  },
+  resolve:{
+        extensions:['.js','.jsx']
+  },
+   module:{
+        rules:[
+			{
+				test: /\.jsx?$/, // test 去判断是否为.js或.jsx,是的话就是进行es6和jsx的编译
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+        ]
+    }
+}
