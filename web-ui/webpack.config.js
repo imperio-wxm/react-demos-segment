92492a528cac1dca438a0919ff2e6e6ad7e63472
wxmimperio <wxmimperio@outlook.com>
2018-01-02 16:48:03 +0800

    add loader

--- web-ui/webpack.config.js
+++ web-ui/webpack.config.js
@@ -1,29 +1,38 @@
 module.exports = {
   devtool: 'eval-source-map',
-  entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
-    filename: "bundle.js"//打包后输出文件的文件名
+    path: __dirname + "/public", //打包后的文件存放的地方
+    filename: "bundle.js" //打包后输出文件的文件名
   },
   devServer: {
-    contentBase: "./public",//本地服务器所加载的页面所在的目录
-    historyApiFallback: true,//不跳转
-    inline: true,//实时刷新
-	port: 8900
+    contentBase: "./public", //本地服务器所加载的页面所在的目录
+    historyApiFallback: true, //不跳转
+    inline: true, //实时刷新
+    port: 8900
   },
-  resolve:{
-        extensions:['.js','.jsx']
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-   module:{
-        rules:[
-			{
-				test: /\.jsx?$/, // test 去判断是否为.js或.jsx,是的话就是进行es6和jsx的编译
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015', 'react']
-				}
-			}
-        ]
-    }
+  module: {
+    rules: [{
+      test: /\.jsx?$/, // test 去判断是否为.js或.jsx,是的话就是进行es6和jsx的编译
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader",
+        options: {
+          modules: true, // 指定启用css modules
+          localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+        }
+      }]
+    }]
+  }
 }
