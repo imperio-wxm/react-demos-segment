b7acaf34b837db44b0083db8aeb4aa3bde07db59
wxmimperio <wxmimperio@outlook.com>
2018-01-09 10:17:04 +0800

    add HBasePanel.js and HDFSPanel.js

---
+++ web-ui/app/js/components/common/hdfs-panel/HDFSPanel.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// 引入react-router模块
+import { Router, Route, Link, hashHistory, IndexRoute, Redirect, IndexLink, browserHistory } from 'react-router'
+
+import HTTPUtil from '../../../actions/fetch/FetchUtils.js'
+
+export default class HDFSPanel extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+       
+    }
+
+    render() {
+        return (
+            <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+                HDFS
+            </div>
+        );
+    }
+}
